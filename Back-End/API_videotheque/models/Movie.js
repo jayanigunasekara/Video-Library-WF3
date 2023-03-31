12e70495e450d7cb7d3e4e5648e2d95a207a440e
Jayani Gunasekara <jayaniacademic@gmail.com>
2023-03-31 09:53:19 +0200

    add new images and new videos

--- Back-End/API_videotheque/models/Movie.js
+++ Back-End/API_videotheque/models/Movie.js
@@ -9,58 +9,58 @@ const movies = [
       "dureeTotale" : 112,
       "duration_viewed":100,
       "imageUrl" : "Asterix.jpg",
-      "videoUrl" : "Incredibles.mp4"
+      "videoUrl" : "video1.mp4"
     },
     {
         "_id" : uuidv4(),
-      "titre" : "Alien",
-      "realisateur" : "Ridley Scott",
-      "description" : "The crew of a commercial spacecraft encounter a deadly lifeform after investigating an unknown transmission.",
+      "titre" : "Soul",
+      "realisateur" : "Pete Docter",
+      "description" : "Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure cum explicabo hic recusandae ipsum voluptatem",
       "dureeTotale" : 117,
       "duration_viewed":100,
-      "imageUrl" : "alien.jpg",
-      "videoUrl" : "The Lion King.mp4"
-    },
-    {
-        "_id" : uuidv4(),
-      "titre" : "Magnolia",
-      "realisateur" : "Paul Thomas Anderson",
-      "description" : "An epic mosaic of interrelated characters in search of love, forgiveness and meaning in the San Fernando Valley.",
-      "dureeTotale" : 188,
-      "duration_viewed":100,
-      "imageUrl" : "magnolia.jpg",
+      "imageUrl" : "soul.jpg",
       "videoUrl" : "Soul.mp4"
     },
     {
         "_id" : uuidv4(),
-      "titre" : "Donnie Darko",
-      "realisateur" : "Richard Kelly",
-      "description" : "After narrowly escaping a bizarre accident, a troubled teenager is plagued by visions of a man in a large rabbit suit who manipulates him to commit a series of crimes.",
-      "dureeTotale" : 113,
+      "titre" : "Shrek",
+      "realisateur" : " Andrew Adamson, Vicky Jenson",
+      "description" : "Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure cum explicabo hic recusandae ipsum voluptatem",
+      "dureeTotale" : 188,
       "duration_viewed":100,
-      "imageUrl" : "donnie_darko.jpg",
+      "imageUrl" : "shrek-Image.jpg",
       "videoUrl" : "Shrek.mp4"
     },
     {
         "_id" : uuidv4(),
-      "titre" : "Dobermann",
-      "realisateur" : "Jan Kounen",
-      "description" : "Dobermann is the world's most ruthless bank robber and with his gang rob bank after bank, now in Paris. What can the police do but to let the mad, morally bankrupt police commissioner loose on him?",
-      "dureeTotale" : 94,
+      "titre" : "Inside Out",
+      "realisateur" : "Pete Docter, Ronnie Del Carmen",
+      "description" : "Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure cum explicabo hic recusandae ipsum voluptatem",
+      "dureeTotale" : 100,
       "duration_viewed":100,
-      "imageUrl" : "dobermann.jpg",
+      "imageUrl" : "Inside-out.png",
       "videoUrl" : "Inside Out.mp4"
     },
     {
         "_id" : uuidv4(),
-      "titre" : "Starship Troopers",
-      "realisateur" : "Paul Verhoeven",
-      "description" : "Humans in a fascist, militaristic future wage war with giant alien bugs.",
+      "titre" : "The Incredibles",
+      "realisateur" : "Brad Bird",
+      "description" : "Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure cum explicabo hic recusandae ipsum voluptatem",
+      "dureeTotale" : 94,
+      "duration_viewed":100,
+      "imageUrl" : "Incredibles.jpg",
+      "videoUrl" : "Incredibles.mp4"
+    },
+    {
+        "_id" : uuidv4(),
+      "titre" : "The Lion King",
+      "realisateur" : "Jon Favreau",
+      "description" : "Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure cum explicabo hic recusandae ipsum voluptatem",
       "dureeTotale" : 129,
       "duration_viewed":100,
-      "imageUrl" : "starship_troopers.jpg",
-      "videoUrl" : "video1.mp4"
-    //   "bgUrl" : "starship_troopers.jpg"
+      "imageUrl" : "Lion King.jpg",
+      "videoUrl" : "The Lion King.mp4"
+ 
     }
 ];
 
